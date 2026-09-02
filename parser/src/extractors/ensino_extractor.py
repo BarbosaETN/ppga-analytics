d9c92a875bc5b7ad141a81d051ce529e3679b25b
Bruno <brunoalexandreoliveira.tech@gmail.com>
2026-09-01 21:12:15 -0300

    reorganização da estrutura de readers e extractors

---
+++ parser/src/extractors/ensino_extractor.py
@@ -0,0 +1,205 @@
+import re
+
+def eh_secao_principal(linha):
+
+    return (
+        abs(linha["x0"] - 64.44) < 2
+        and linha["fontname"] == "FAAAAA+Tahoma"
+        and abs(linha["size"] - 11.75) < 0.2
+        and linha["non_stroking_color"] == (
+            0.1961,
+            0.4235,
+            0.6
+        )
+    )
+
+def eh_secao_atuacao_profissional(linha):
+    return linha["text"].strip().lower() == "atuação profissional"
+
+def eh_instituicao(linha):
+
+    return (
+        abs(linha["x0"] - 107.42) < 2
+        and linha["fontname"] == "GAAAAA+Tahoma-Bold"
+        and abs(linha["size"] - 7.83) < 0.2
+        and linha["non_stroking_color"] == (
+            0.6706,
+            0.6706,
+            0.6706
+        )
+    )
+
+
+def eh_subsecao(linha):
+
+    return (
+        abs(linha["x0"] - 151.72) < 2
+        and linha["fontname"] == "GAAAAA+Tahoma-Bold"
+        and abs(linha["size"] - 7.83) < 0.2
+        and linha["non_stroking_color"] == (
+            0.4,
+            0.4,
+            0.4
+        )
+    )
+
+
+def eh_periodo(linha):
+
+    texto = linha["text"].strip()
+
+    padrao = r"^\d{4}\s*-\s*(Atual|\d{4})$"
+
+    return (
+        re.match(
+            padrao,
+            texto,
+            re.IGNORECASE
+        )
+        and abs(linha["x0"] - 151.72) < 2
+    )
+
+
+def eh_outras_informacoes(linha):
+
+    return (
+        linha["text"]
+        .strip()
+        .lower()
+        == "outras informações"
+    )
+
+
+def eh_conteudo(linha):
+
+    return abs(linha["x0"] - 210.90) < 3
+
+
+def extrair_atuacao_profissional(linhas):
+
+    resultado = []
+
+    instituicao_atual = None
+    subsecao_atual = None
+    periodo_atual = None
+    subsecao_interna_atual = None
+
+    for linha in linhas:
+
+        # ====================================================
+        # INSTITUIÇÃO
+        # ====================================================
+
+        if eh_instituicao(linha):
+
+            instituicao_atual = {
+                "tipo": "instituicao",
+                "texto": linha["text"],
+                "subsecoes": []
+            }
+
+            resultado.append(instituicao_atual)
+
+            subsecao_atual = None
+            periodo_atual = None
+            subsecao_interna_atual = None
+
+            continue
+
+
+        # ====================================================
+        # SUBSEÇÃO
+        # Ex.: Vínculo institucional
+        #      Atividades
+        # ====================================================
+
+        if eh_subsecao(linha):
+
+            if instituicao_atual is None:
+                continue
+
+            subsecao_atual = {
+                "tipo": "subsecao",
+                "texto": linha["text"],
+                "periodos": []
+            }
+
+            instituicao_atual["subsecoes"].append(
+                subsecao_atual
+            )
+
+            periodo_atual = None
+            subsecao_interna_atual = None
+
+            continue
+
+
+        # ====================================================
+        # PERÍODO
+        # Ex.: 2022 - Atual
+        #      2008 - 2010
+        # ====================================================
+
+        if eh_periodo(linha):
+
+            if subsecao_atual is None:
+                continue
+
+            periodo_atual = {
+                "tipo": "periodo",
+                "texto": linha["text"],
+                "conteudos": [],
+                "subsecoes": []
+            }
+
+            subsecao_atual["periodos"].append(
+                periodo_atual
+            )
+
+            subsecao_interna_atual = None
+
+            continue
+
+
+        # ====================================================
+        # SUBSEÇÃO INTERNA
+        # Ex.: Outras informações
+        # ====================================================
+
+        if eh_outras_informacoes(linha):
+
+            if periodo_atual is None:
+                continue
+
+            subsecao_interna_atual = {
+                "tipo": "subsubsecao",
+                "texto": linha["text"],
+                "conteudos": []
+            }
+
+            periodo_atual["subsecoes"].append(
+                subsecao_interna_atual
+            )
+
+            continue
+
+
+        # ====================================================
+        # CONTEÚDO
+        # ====================================================
+
+        if eh_conteudo(linha):
+
+            if subsecao_interna_atual is not None:
+
+                subsecao_interna_atual["conteudos"].append(
+                    linha["text"]
+                )
+
+            elif periodo_atual is not None:
+
+                periodo_atual["conteudos"].append(
+                    linha["text"]
+                )
+
+    return resultado
--- parser/src/main.py
+++ parser/src/main.py
@@ -1,13 +1,14 @@
 from pathlib import Path
-
-from readers.pdf_reader import (
-    extrair_linhas,
+from readers.pdf_reader import extrair_linhas
+from extractors.ensino_extractor import (
+    eh_secao_principal,
     eh_instituicao,
     eh_subsecao,
     eh_periodo,
     eh_outras_informacoes,
     eh_conteudo
 )
+
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -25,21 +26,15 @@ linhas = extrair_linhas(PDF_PATH)
 
 for linha in linhas:
 
-    # Apenas para teste com menos páginas
-    if linha["pagina"] > 10:
-        break
+    texto = linha["text"].strip().lower()
 
-    if (
-        eh_instituicao(linha)
-        or eh_subsecao(linha)
-        or eh_periodo(linha)
-        or eh_outras_informacoes(linha)
-        or eh_conteudo(linha)
-    ):
-
+    if eh_secao_principal(linha):
         print(
             f"PÁGINA {linha['pagina']} | "
             f"{linha['text']} | "
             f"x0={linha['x0']:.2f} | "
-            f"top={linha['top']:.2f}"
+            f"top={linha['top']:.2f} | "
+            f"font={linha['fontname']} | "
+            f"size={linha['size']:.2f} | "
+            f"cor={linha['non_stroking_color']}"
         )
